855c2b7184c7d7855f92a59aca5e5f60c005e967
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-16 08:56:06 +0100

    Adderd email to the schema for auth

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: 'Required' },
   lastName: { type: String, required: 'Required' },
   username: { type: String, required: 'Required' },
+  email: { type: String, required: 'Required' },
   password: { type: String, required: 'Required' }
 });
 
