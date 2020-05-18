7512cadf060ba2c50e82568528b72c0592c05c78
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-18 14:01:48 +0100

    Refactored auth for registration

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,8 @@ let router = express.Router();
 const User = require('../models/userSchema');
 const Token = require('../models/token');
 
+let hashedPass;
+
 mongoose.connect('mongodb+srv://gadaput:gadaput231%23%23@cluster0-5kzif.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
@@ -26,93 +28,96 @@ db.once('open', () => {
   console.log('Database connection succeeded for auth');
 });
 
-router.get('/register', (req, res) => {
-  res.send('The registration page');
-});
+router.get('/register', (req, res) => res.status(200).send('The registration page'));
 
-router.post('/register', (req, res) => {
-  check('username', 'UserName cannot be blank').notEmpty();
-  check('email', 'Email is not valid').isEmail();
-  check('email', 'Email cannot be blank').notEmpty();
-  check('password', 'Password must be at least 4 characters long').isLength({ min: 4 });
-  check('email').normalizeEmail({ remove_dots: false });
+router.post(
+  '/register',
+  [
+    check('username', 'UserName cannot be blank').notEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('email', 'Email cannot be blank').notEmpty(),
+    check('password', 'Password must be at least 4 characters long').isLength({ min: 4 }),
+    check('email').normalizeEmail({ remove_dots: false })
+  ],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+    } else {
+      // Make sure this account doesn't already exist
+      User.findOne({ email: req.body.email }, (err, user) => {
+        // Make sure user doesn't already exist
+        if (user) {
+          res
+            .status(400)
+            .send({ msg: 'The email address you have entered is already associated with another account.' });
+        } else {
+          // Hash password
+          // eslint-disable-next-line no-shadow
+          bcrypt.hash(req.body.password, null, null, (err, hash) => {
+            if (err) {
+              res.send(err);
+            }
+            hashedPass = hash;
+            // console.log(hashedPass);
 
-  // Check for validation errors
-  const errors = validationResult(req);
-  if (errors) {
-    return res.status(400).send(errors);
-  }
+            // req.body.password = hashedPassword;
+            // Create and save the user
+            // eslint-disable-next-line no-param-reassign
+            user = new User({
+              email: req.body.email,
+              username: req.body.username,
+              password: hashedPass,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName
+            });
+            // eslint-disable-next-line no-shadow
+            user.save((err) => {
+              if (err) {
+                res.status(500).send({ msg: err.message });
+              } else {
+                // Create a verification token for this user
+                const token = new Token({ _userId: user.id, token: crypto.randomBytes(16).toString('hex') });
 
-  // Make sure this account doesn't already exist
-  User.findOne({ email: req.body.email }, (err, user) => {
-    // Make sure user doesn't already exist
-    if (user) {
-      return res
-        .status(400)
-        .send({ msg: 'The email address you have entered is already associated with another account.' });
-    }
-
-    // Hash password
-    // eslint-disable-next-line no-shadow
-    bcrypt.hash(req.body.password, null, null, (err, hash) => {
-      if (err) {
-        res.send(err);
-      }
-      return hash;
-    });
-    // req.body.password = hashedPassword;
-    // Create and save the user
-    // eslint-disable-next-line no-param-reassign
-    user = new User({
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName
-    });
-    // eslint-disable-next-line no-shadow
-    user.save((err) => {
-      if (err) {
-        return res.status(500).send({ msg: err.message });
-      }
-
-      // Create a verification token for this user
-      const token = new Token({ _userId: user.id, token: crypto.randomBytes(16).toString('hex') });
-
-      // Save the verification token
-      // eslint-disable-next-line no-shadow
-      token.save((err) => {
-        if (err) {
-          return res.status(500).send({ msg: err.message });
+                // Save the verification token
+                // eslint-disable-next-line no-shadow
+                token.save((err) => {
+                  if (err) {
+                    res.status(500).send({ msg: err.message });
+                  } else {
+                    // Send the email
+                    const transporter = nodemailer.createTransport({
+                      service: 'Sendgrid',
+                      // eslint-disable-next-line max-len
+                      auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD }
+                    });
+                    const mailOptions = {
+                      from: 'no-reply@gadaput.com',
+                      to: user.email,
+                      subject: 'Account Verification Token',
+                      text: `${'Hello,\n\n Please verify your account by clicking the link: \nhttp://'}${
+                        req.headers.host
+                      }/auth/confirmation/${token.token}.\n`
+                    };
+                    // eslint-disable-next-line no-shadow
+                    transporter.sendMail(mailOptions, (err) => {
+                      if (err) {
+                        return res.status(500).send({ msg: err.message });
+                      }
+                      return res.status(200).send(`A verification email has been sent to ${user.email}.`);
+                    });
+                  }
+                });
+              }
+            });
+          });
         }
-
-        // Send the email
-        const transporter = nodemailer.createTransport({
-          service: 'Sendgrid',
-          auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD }
-        });
-        const mailOptions = {
-          from: 'no-reply@yourwebapplication.com',
-          to: user.email,
-          subject: 'Account Verification Token',
-          text: `${'Hello,\n\n Please verify your account by clicking the link: \nhttp://'}${
-            req.headers.host
-          }/confirmation/${token.token}.\n`
-        };
-        // eslint-disable-next-line no-shadow
-        transporter.sendMail(mailOptions, (err) => {
-          if (err) {
-            return res.status(500).send({ msg: err.message });
-          }
-          res.status(200).send(`A verification email has been sent to ${user.email}.`);
-          return res.status(200);
-        });
-        return res.status(200);
       });
-      return res.status(200);
-    });
-    return res.status(200);
-  });
-  return res.status(200);
-});
+    }
+  }
+);
+
+router.get('/login', (req, res) => res.status(200).send('The login page'));
+
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dbconnection = require('./models');
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
